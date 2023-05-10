85536e99888400dfdc0140a06dbd329f5ec584e3
RitulYadav <ritulsinghyadav302@gmail.com>
2023-05-10 10:32:52 +0530

    ..

--- frontend/src/components/main/Navbar.js
+++ frontend/src/components/main/Navbar.js
@@ -3,47 +3,47 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div><nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-        <img src="node-js.png"
-        height={20}
-        />
-        Navbar
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i className="fas fa-bars" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/home">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/Login">
-              Login
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#Signup">
-              Signup
-            </a>
-          </li>
-          
-        </ul>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src="node-js.png"
+            height={20}
+          />
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fas fa-bars" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Login">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#Signup">
+                Signup
+              </a>
+            </li>
+
+          </ul>
+        </div>
       </div>
+    </nav>
     </div>
-  </nav>
-  </div>
   )
 }
 
